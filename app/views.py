a4b3925c51ba68fc4cffa311c993178b8af1e021
Clarence Lam <clarencelam95@gmail.com>
2016-10-23 16:05:53 -0700

    connected to mail

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import Flask, render_template, redirect, url_for, request, flash
-from forms import ContactForm
+
+# from forms import ContactForm
+# from flask_mail import Message
 
 app.secret_key = 'development key'
 
@@ -12,19 +14,6 @@ def home():
       return redirect(url_for('contact'))
   return render_template('index.html')
 
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-  form = ContactForm()
-
-  if request.method == 'POST':
-    if form.validate() == False:
-      return render_template('contact.html', form=form)
-    else:
-      return render_template('contact.html', success=True)
-
-  elif request.method == 'GET':
-    return render_template('contact.html', form=form)
 
 @app.route('/teams')
 def teams():
